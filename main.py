9c4e4ba1e5bab2a1f0bbfeffe8b57d339f7ff6c3
joltzen <jasonoltzen@web.de>
2024-07-05 00:15:40 +0200

    Add visualization helpers

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from mirror_image_method import MirrorImageMethod
 from visualization import MeshVisualizer
 from utils import Target
 
+
 def main():
     mesh_file_path = "./model/simple_cube_normals_flipped.obj"
     source_point = np.array([0.123, 0.2, 0.113])
-    target_face = 5
+    #target_face = 5
     target_position = Target.generate_random_coordinates()
     target = Target(target_position, 0.1)
     reflections_order = 10
 
     room = MirrorImageMethod(mesh_file_path, source_point, target, reflections_order)
-    visualizer = MeshVisualizer(room, target_face)
+    visualizer = MeshVisualizer(room)
     # room.calculatePaths():
     visualizer.plot_mesh()
 
--- visualization.py
+++ visualization.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from utils import Target
+import matplotlib.cm as cm
 
 class MeshVisualizer:
-    def __init__(self, room, target_face):
+    def __init__(self, room, target_face=-1):
         self.room = room
         self.target_face = target_face
 
@@ -14,9 +15,14 @@ class MeshVisualizer:
         ax.set_box_aspect([1, 1, 1])
 
         self.plot_vertices(ax)
-        self.plot_faces(ax)
         self.plot_image_sources(ax)
         self.plot_reflections(ax)
+        if(self.target_face >= 0):
+            self.plot_highlighted_target_face(ax)
+        else: 
+            self.plot_faces(ax)
+        # plot the faces with their index
+        #self.identify_faces(ax)
 
         ax.set_axis_off()
         plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
@@ -32,7 +38,18 @@ class MeshVisualizer:
         ax.text(self.room.source[0], self.room.source[1], self.room.source[2], "S", color="blue")
 
     def plot_faces(self, ax):
-        """Plot the faces of the mesh."""
+        """Plot the faces of the mesh."""        
+        mesh = self.room.mesh
+        for index, face in enumerate(mesh.faces):
+            for i in range(3):
+                ax.plot([mesh.vertices[face[i], 0], mesh.vertices[face[(i + 1) % 3], 0]],
+                        [mesh.vertices[face[i], 1], mesh.vertices[face[(i + 1) % 3], 1]],
+                        [mesh.vertices[face[i], 2], mesh.vertices[face[(i + 1) % 3], 2]], c="black", alpha=0.1 )
+            mirrored_ps = self.room.mirror_source(self.room.source, face)
+            ax.scatter(mirrored_ps[0], mirrored_ps[1], mirrored_ps[2], c="pink" if index != self.target_face else "orange")
+    
+    def plot_highlighted_target_face(self, ax):
+        """Plot the faces of the mesh."""        
         mesh = self.room.mesh
         for index, face in enumerate(mesh.faces):
             color = "black" if index != self.target_face else "red"
@@ -43,12 +60,30 @@ class MeshVisualizer:
             mirrored_ps = self.room.mirror_source(self.room.source, face)
             ax.scatter(mirrored_ps[0], mirrored_ps[1], mirrored_ps[2], c="pink" if index != self.target_face else "orange")
 
+    def identify_faces(self, ax):
+        """Colors the faces and labels them with their index."""
+        mesh = self.room.mesh
+        num_faces = len(mesh.faces)
+        colors = plt.get_cmap('tab20', num_faces)
+
+        for index, face in enumerate(mesh.faces):
+            color = colors(index / num_faces)
+            for i in range(3):
+                ax.plot([mesh.vertices[face[i], 0], mesh.vertices[face[(i + 1) % 3], 0]],
+                        [mesh.vertices[face[i], 1], mesh.vertices[face[(i + 1) % 3], 1]],
+                        [mesh.vertices[face[i], 2], mesh.vertices[face[(i + 1) % 3], 2]], c=color, alpha=0.6 if index != self.target_face else 1.0)
+            
+            centroid = self.room.centroid_of_face(face)
+            ax.text(centroid[0], centroid[1], centroid[2], str(index), color=color)
+            mirrored_ps = self.room.mirror_source(self.room.source, face)
+            ax.scatter(mirrored_ps[0], mirrored_ps[1], mirrored_ps[2], color=color)
+
     def plot_image_sources(self, ax):
         """Plot the image sources."""
         mesh = self.room.mesh
         ps = self.room.source
 
-        if self.target_face < len(mesh.faces):
+        if 0 <= self.target_face < len(mesh.faces):
             face = mesh.faces[self.target_face]
             center = self.room.centroid_of_face(face)
             r = center - ps
@@ -56,19 +91,23 @@ class MeshVisualizer:
             orthogonal = np.dot(r, normal) * normal
             mirrored_source = self.room.mirror_source(ps, face)
 
-            # ax.quiver(ps[0], ps[1], ps[2], r[0], r[1], r[2], color="green", alpha=0.2)
-            # ax.text(ps[0] + r[0]/2, ps[1] + r[1]/2, ps[2] + r[2]/2, 'R', color='green')
-            # ax.quiver(center[0], center[1], center[2], normal[0], normal[1], normal[2], color="red", alpha=0.2)
-            # ax.text(center[0] + normal[0]/2, center[1] + normal[1]/2, center[2] + normal[2]/2, 'n', color='red')
-            # ax.quiver(ps[0], ps[1], ps[2], orthogonal[0], orthogonal[1], orthogonal[2], color="purple", alpha=0.2)
-            # ax.text(ps[0] + orthogonal[0]/2, ps[1] + orthogonal[1]/2, ps[2] + orthogonal[2]/2, 'n|r|cos(alpha)', color='purple')
-            # ax.scatter(mirrored_source[0], mirrored_source[1], mirrored_source[2], c="orange", alpha=0.2)
-            # ax.text(mirrored_source[0], mirrored_source[1], mirrored_source[2], 'I', color='orange')
+            self.plot_vector(ax, ps, r, 'green', 'R')
+            self.plot_vector(ax, center, normal, 'red', 'n')
+            self.plot_vector(ax, ps, orthogonal, 'purple', 'n|r|cos(alpha)')
+            ax.scatter(mirrored_source[0], mirrored_source[1], mirrored_source[2], c="orange", alpha=0.2)
+            ax.text(mirrored_source[0], mirrored_source[1], mirrored_source[2], 'I', color='orange')
+
+    def plot_vector(self, ax, origin, vector, color, label):
+        """Helper function to plot vectors with labels."""
+        ax.quiver(origin[0], origin[1], origin[2], vector[0], vector[1], vector[2], color=color, alpha=0.3)
+        ax.text(origin[0] + vector[0]/2, origin[1] + vector[1]/2, origin[2] + vector[2]/2, label, color=color)
 
     def plot_reflections(self, ax):
+        """Plot reflection paths."""
         paths_dict = self.room.paths
+        total_paths = sum(len(paths) for paths in paths_dict.values())
         print("-" * 80)
-        print(sum(len(paths) for paths in paths_dict.values()), "paths found.")
+        print(f"{total_paths} paths found.")
 
         order_colors = ['blue', 'red', 'green', 'purple', 'orange', 'cyan', 'magenta']
         hit_colors = ['red', 'green', 'purple', 'orange', 'cyan', 'magenta', 'blue']
@@ -80,26 +119,26 @@ class MeshVisualizer:
                 print(f"Travel time for order {order}: {travel_time:.6f} seconds")
                 print("=" * 80)
                 for ray_info in path.rays:
-                    origin = ray_info["origin"]
-                    direction = ray_info["direction"]
-                    reflection_point = ray_info["reflection_point"]
-
                     self.print_ray_info(ray_info)
-
-                    color = order_colors[order % len(order_colors)]
-                    hit_color = hit_colors[order % len(hit_colors)]
-                    ax.quiver(origin[0], origin[1], origin[2], direction[0], direction[1], direction[2], color=color)
-
-                    if reflection_point is not None:
-                        ax.scatter(reflection_point[0], reflection_point[1], reflection_point[2], c=hit_color)
+                    self.plot_ray(ax, ray_info, order_colors[order % len(order_colors)], hit_colors[order % len(hit_colors)])
 
         if any(paths_dict.values()):
             ax.scatter(self.room.target.position[0], self.room.target.position[1], self.room.target.position[2], color="magenta", label="Target", s=self.room.target.radius * 1000)
         else:
             print("No rays hit the target.")
-            
 
+    def plot_ray(self, ax, ray_info, color, hit_color):
+        """Plot individual rays."""
+        origin = ray_info["origin"]
+        direction = ray_info["direction"]
+        reflection_point = ray_info["reflection_point"]
+        ax.quiver(origin[0], origin[1], origin[2], direction[0], direction[1], direction[2], color=color)
+        if reflection_point is not None:
+            ax.scatter(reflection_point[0], reflection_point[1], reflection_point[2], c=hit_color)
+
+            
     def print_ray_info(self, ray_info):
+        """Print ray information."""
         origin = ray_info["origin"]
         direction = ray_info["direction"]
         reflection_point = ray_info["reflection_point"]
